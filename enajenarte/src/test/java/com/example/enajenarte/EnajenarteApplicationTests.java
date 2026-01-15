b21ea7342cf6140375867b9c35e1c7fc993a6726
PCaam-git <pcaamano@outlook.es>
2026-01-15 18:51:04 +0100

    Initial configuration Spring Boot project

---
+++ enajenarte/src/test/java/com/example/enajenarte/EnajenarteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enajenarte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnajenarteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
