d39116662a930a0af9843ccd95d7223b38cc00e9
PCaam-git <pcaamano@outlook.es>
2026-01-16 11:01:09 +0100

    package path modification in EnajenarteApplication

--- enajenarte/src/main/java/com/svalero/enajenarte/EnajenarteApplication.java
+++ enajenarte/src/main/java/com/svalero/enajenarte/EnajenarteApplication.java
@@ -1,4 +1,4 @@
-package com.example.enajenarte;
+package com.svalero.enajenarte;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
