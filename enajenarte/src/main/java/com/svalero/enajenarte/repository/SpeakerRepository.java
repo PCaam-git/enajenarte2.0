18a3d90e07ff9815948f6d9985192e95161bcbc7
PCaam-git <pcaamano@outlook.es>
2026-01-19 10:14:28 +0100

    repository: queries for mandatory filters

--- enajenarte/src/main/java/com/svalero/enajenarte/repository/SpeakerRepository.java
+++ enajenarte/src/main/java/com/svalero/enajenarte/repository/SpeakerRepository.java
@@ -8,5 +8,11 @@ import java.util.List;
 
 @Repository
 public interface SpeakerRepository extends CrudRepository<Speaker, Long> {
+
     List<Speaker> findAll();
+
+    // Filtros (3 campos)
+    List<Speaker> findBySpecialityContainingIgnoreCase(String speciality);
+    List<Speaker> findByAvailable(boolean available);
+    List<Speaker> findByYearsExperience(int yearsExperience);
 }
--- enajenarte/src/main/java/com/svalero/enajenarte/repository/UserRepository.java
+++ enajenarte/src/main/java/com/svalero/enajenarte/repository/UserRepository.java
@@ -8,5 +8,11 @@ import java.util.List;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+
     List<User> findAll();
+
+    // Filtros (3 campos)
+    List<User> findByUsernameContainingIgnoreCase(String username);
+    List<User> findByEmailContainingIgnoreCase(String email);
+    List<User> findByActive(boolean active);
 }
